03d635975c3b4d87e1d409b9c320846994c66f90
Daniel Gempesaw <gempesaw@gmail.com>
2016-04-07 10:59:39 -0400

    Add skeleton of jira search functionality

---
+++ test/jira_test.js
@@ -0,0 +1,12 @@
+import { expect } from 'chai';
+
+describe('Flowbot', () => {
+
+    describe('jira lookup', () => {
+
+        it('should search for the most recent ticket', () => {
+
+        });
+
+    });
+});
