957fc4c453c05401ee468dab8ae2f8fcf5ee1b11
Daniel Gempesaw <gempesaw@gmail.com>
2016-08-10 16:50:04 -0400

    Save a jira ticket from message content

--- test/jira_test.js
+++ test/jira_test.js
@@ -1,12 +1,20 @@
 import { expect } from 'chai';
+import { handleJira, getLastTicket } from '~/lib/commands/jira';
 
-describe('Flowbot', () => {
-
-    describe('jira lookup', () => {
-
-        it('should search for the most recent ticket', () => {
-
-        });
-
+describe('jira lookup', () => {
+    let messageWithTicket;
+    beforeEach( () => {
+        messageWithTicket = {
+            thread_id: 'threadId',
+            event: 'message',
+            flow: 'flowId',
+            content: 'SC-12345'
+        };
     });
+
+    it('should persist the most recently mentioned ticket', () => {
+        handleJira(messageWithTicket);
+        expect(getLastTicket()).to.equal('SC-12345');
+    });
+
 });
