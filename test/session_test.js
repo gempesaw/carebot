3bbd7effd7ec517a8a28f6d5cc1450b8e3b14569
Daniel Gempesaw <gempesaw@gmail.com>
2016-09-21 02:05:22 -0400

    Add tests for sending messages

--- test/session_test.js
+++ test/session_test.js
@@ -1,11 +1,41 @@
 import { expect } from 'chai';
-import { respondToMessages } from '~/lib/session';
+import { respondToMessages, sendMessage } from '~/lib/session';
 import config from '~/lib/config';
+import * as td from 'testdouble';
 
 describe('Session', () => {
     it('should ignore messages from the bot', () => {
-        let event = { user: config.flowdockUserId };
-        let reply = respondToMessages(undefined, event);
+        const event = { user: config.flowdockUserId };
+        const reply = respondToMessages(undefined, event);
         expect(reply).to.match(/ignoring our own/i);
     });
+
+    describe('sending messages', () => {
+        let session, flow, reply;
+        beforeEach(() => {
+            session = td.object(['comment', 'privateMessage']);
+            flow = 'flow';
+            reply = 'reply';
+        });
+
+        it('should send comments to flows', () => {
+            // an event from a flow has an event.flow and event.id for
+            // the parent message
+            const id = 'id';
+            const event = { flow, id };
+            sendMessage(session, event, reply);
+            td.verify(session.comment(flow, id, reply));
+        });
+
+        it('should send PMs to 1 on 1s', () => {
+            const user = 'user';
+            // one on ones do not have event.flow property, but they
+            // do have an event.user
+            sendMessage(session, { user }, reply);
+            td.verify(session.privateMessage(user, reply));
+        });
+
+    });
+
+
 });
