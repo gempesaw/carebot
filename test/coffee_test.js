a63c5ddc689cb73b0e88cd98493786195ed89f26
Daniel Gempesaw <gempesaw@gmail.com>
2016-09-30 09:12:41 -0400

    Externalize coffee data to a json file

--- test/coffee_test.js
+++ test/coffee_test.js
@@ -1,9 +1,16 @@
 import { expect } from 'chai';
-import { pickCoffee } from '~/lib/commands/coffee';
+import fsp from 'fs-promise';
+
+import { pickCoffee, readCoffeeChoices } from '~/lib/commands/coffee';
 
 describe('Coffee', () => {
     it('should pick a random coffee', async function () {
         let reply = await pickCoffee([{ imageUrl: 'url' }]);
         expect(reply).to.equal('url');
+    });
+
+    it('should read the coffee from file', async function () {
+        let coffeeData = await readCoffeeChoices();
+        expect(coffeeData).to.be.an.array;
     });
 });
