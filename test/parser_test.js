5467b89d768bc1542851bdd5a5bb64b531de7095
Daniel Gempesaw <gempesaw@gmail.com>
2016-10-01 20:57:06 -0400

    Pass through arguments when parsing commands

--- test/parser_test.js
+++ test/parser_test.js
@@ -2,28 +2,35 @@ import { expect } from 'chai';
 import parseAction from '~/lib/parser';
 
 describe('Parsing commands', () => {
-    let commands;
+    let commands, expected;
     beforeEach(() => {
+        expected = 'value';
         commands = {
-            key: 'value'
+            key: expected
         };
     });
 
     it('should parse exact string matches', () => {
-        const content = 'key';
-        const assert = parseAction(`.${content}`, commands);
-        expect(assert).to.equal(commands[content]);
+        const content = '.key';
+        const assert = parseAction(content, commands);
+        expect(assert).to.equal(expected);
     });
 
     it('should ignore case during matches', () => {
-        const content = 'KeY';
-        const assert = parseAction(`.${content}`, commands);
-        expect(assert).to.equal(commands[content.toLowerCase()]);
+        const content = '.KeY';
+        const assert = parseAction(content, commands);
+        expect(assert).to.equal(expected);
     });
 
     it('should ignore surrounding whitespace', () => {
         const content = '.key     ';
         const assert = parseAction(content, commands);
-        expect(assert).to.equal(commands.key);
+        expect(assert).to.equal(expected);
+    });
+
+    it('should pass through arguments', () => {
+        const content = '.key argument';
+        const assert = parseAction(content, commands);
+        expect(assert).to.equal(expected);
     });
 });
