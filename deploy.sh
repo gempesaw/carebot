be892f363b2a568fe85ddf24733464c96ae52d8e
Daniel Gempesaw <gempesaw@gmail.com>
2017-01-20 16:21:52 -0500

    (self-deploy) Handle extra details during auto deploy

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,10 @@
 set -e
 SEED=$(date +%Y-%m-%d:%H:%M:%S)
+rm lib/config_backup_*.js
 cp lib/config.js lib/config_backup_$SEED.js
 git fetch origin
 git reset --hard origin/master
+npm install
 npm test
 cp lib/config_backup_$SEED.js lib/config.js
 
