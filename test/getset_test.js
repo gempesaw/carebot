c11b89e7ccaefcb85a36dc0ecc5d704b947cec2e
Daniel Gempesaw <gempesaw@gmail.com>
2017-03-01 00:35:41 -0500

    (getset) Use async with arrow functions c: c: c:

--- test/getset_test.js
+++ test/getset_test.js
@@ -8,13 +8,13 @@ describe('Get Set!', () => {
     const dest = (key = 'testKey') => path.join(__dirname, `../data/${key}`);
     beforeEach(() => fsp.writeFile(dest(), 'test data'));
 
-    it('should return data', async function () {
+    it('should return data', async () => {
         const data = await getSet.get({ content: '.get testKey' });
         expect(data).to.equal('test data');
         expect(getSet.get({ content: '.get testKey' })).to.be.a('Promise');
     });
 
-    it('should store data for retrieval', async function () {
+    it('should store data for retrieval', async () => {
         const result = await getSet.set({ content: '.set setTestKey set test data' });
         expect(result).to.match(/Okay/);
 
@@ -22,36 +22,37 @@ describe('Get Set!', () => {
         expect(storedData).to.equal('set test data');
     });
 
-    it('should fail gracefully when file is missing', async function () {
+    it('should fail gracefully when file is missing', async () => {
         const data = await getSet.get({ content: '.get missingKey' });
         expect(data).to.not.be.an('Error');
         expect(data).to.include('Invalid key');
     });
 
-    it('should only use alphanumeric chars for key', async function () {
+    it('should only use alphanumeric chars for key', async () => {
         const result = await getSet.set({ content: '.set ../hee/../hee/\#$%^&5*/ data'});
         expect(result).to.match(/heehee5/);
 
         expect(await getSet.get({ content: '.get heehee5' })).to.equal('data');
     });
 
-    it('should list all available keys', async function () {
+    it('should list all available keys', async () => {
         const all = await getSet.getAll({ content: '.getAll' });
         expect(all).to.include('heehee5');
         expect(all).to.include('setTestKey');
         expect(all).to.include('testKey');
     });
 
-    it('should unset a key', async function () {
+    it('should unset a key', async () => {
         await getSet.set({ content: '.set unsetKey unset data' });
         const unset = await getSet.unset({ content: '.unset unsetKey' });
         const all = await getSet.getAll();
         expect(all).to.not.include('unsetKey');
     });
 
-    // after(async function () {
-    //     const files = [ 'testKey', 'setTestKey', 'heehee5' ].map(dest);
-    //     await files.map(async function (it) { await fsp.unlink(it); });
-    // });
+    after(async () => {
+        const files = [ 'testKey', 'setTestKey', 'heehee5' ]
+              .map(dest);
+        await files.map(async (it) => { await fsp.unlink(it); });
+    });
 
 });
