04b42e5e499ae05f8c4dfc895d8757b8a4e8c455
Daniel Gempesaw <gempesaw@gmail.com>
2017-04-26 14:16:00 -0400

    Fix cupcake screenshot validation
    With more string based weird testing

--- test/cupcake_test.js
+++ test/cupcake_test.js
@@ -1,5 +1,7 @@
 import { expect } from 'chai';
 import handleCommand from '~/lib/commands';
+import { getEnhancedCupcake } from '~/lib/commands/cupcake';
+import fsp from 'fs-promise';
 
 describe('Cupcake lookup', () => {
     it('should respond with the cupcake thumbnail', (done) => {
@@ -11,9 +13,12 @@ describe('Cupcake lookup', () => {
     });
 
     // too lazy to set up chromedriver on travis
-    // it('should provide an enhanced cupcake screenshot', (done) => {
-    //     handleCommand({ content: '.enhance' }).then(reply => {
-    //         done();
-    //     });
+    // it('should provide an enhanced cupcake screenshot', async () => {
+    //     const screenshot = await getEnhancedCupcake();
+    //     const png = new Buffer(screenshot.data, 'base64')
+    //           .toString('binary');
+    //     await fsp.writeFile('cupcake.png', png, 'binary');
     // }).timeout(10000);
+
+
 });
