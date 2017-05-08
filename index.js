0f8a9dc283151eeab0a526a1f4303057f4cb303a
Daniel Gempesaw <gempesaw@gmail.com>
2017-05-08 18:26:45 -0400

    Parse request body with koa

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 import Koa from 'koa';
 import _ from 'koa-route';
+import bodyParser from 'koa-bodyparser';
 
 import config from '~/lib/config';
 // import { respondToMessages } from '~/lib/session';
 
 const app = new Koa();
+app.use(bodyParser());
 
 app.use(_.get('/', (ctx) => {
     ctx.body = 'ok';
 }));
+
 
 app.use(_.post('/flowbot', (ctx) => {
     console.log(ctx.request);
